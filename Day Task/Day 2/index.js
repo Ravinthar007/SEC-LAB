11a38a737bdd853e0cc6b97e7f39ace261b510a0
RAVINTHAR S <104417627+Ravinthar007@users.noreply.github.com>
2025-07-11 10:47:21 +0530

    Add files via upload

---
+++ Day Task/Day 2/index.js
@@ -0,0 +1,13 @@
+var x = 3;
+let name = `js`;
+const iscloudy = true;
+console.log(x+" "+name+" "+iscloudy);
+document.write(name+" ");
+document.write(x+" ");
+document.write(iscloudy);
+function add(a,b){
+    console.log("This is a function "+(a+b));
+}
+add(4,5);
+int a=parseInt("Enter the value: ");
+int b=parseInt("Enter the sceond value: ");
